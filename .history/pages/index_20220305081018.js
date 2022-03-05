537c5b3cbaccf3a2ea9f5730f7fb2a368ae41926
apeli23 <apelibrian@gmail.com>
2022-03-05 12:06:15 +0300

    bugs

---
+++ .history/pages/index_20220305081018.js
@@ -0,0 +1,117 @@
+
+import { forwardRef, useState, createRef, useEffect, useRef } from 'react';
+import UserDetails from '../components/UserDetails';
+import { Black, Button, Details, FormContainer, Input, Inputs, Label, Blue, Card, Card_1, Card_2, ColorGrid, Container, Dots, Green, Pink, Red1, Red2, Templates, Text, Title, TitleContainer, Yellow, FinalCardContainer, Front, InfoGrid, NameTag, Back, Name, Brand, CardText, Address, PhoneNumber, Email, TextLg, TextSm, UploadButto, Backn, UploadButton } from "../styles/emotion";
+import { FaLaptop, FaMapMarkerAlt, FaPhone } from 'react-icons/fa';
+import { useScreenshot } from 'use-react-screenshot';
+import html2canvas from 'html2canvas';
+
+export default function Home() {
+  const [selectedtemplate, setSelectedTemplate] = useState(false)
+  const [submittedform, setSubmittedForm] = useState(false);
+  const [finalcard, setFinalCard] = useState();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phonenumber, setPhoneNumber] = useState('');
+  const [location, setLocation] = useState('');
+  const [website, setWebsite] = useState('');
+  const [brand, setBrand] = useState('');
+  const [link, setLink] = useState('');
+  const [userprofile, takeScreenshot] = useScreenshot();
+
+  const refOne = createRef();
+  const refTwo = createRef();
+  const finalcardRef = useRef();
+
+  const $ = require('jquery')
+
+  let backgroundURL = ""
+  const Card1 = forwardRef((props, ref) => (
+    <Card_1 ref={refOne} id="card" onClick={template1Handler} >
+      <ColorGrid>
+        <Black />
+        <Red1 />
+        <Red2 />
+        <Green />
+      </ColorGrid>
+    </Card_1>
+  ));
+
+  const Card2 = forwardRef((props, ref) => (
+    <Card_2 ref={refTwo} onClick={template2Handler}>
+      <Blue />
+      <Yellow />
+      <Pink />
+      <Dots />
+    </Card_2>
+  ));
+
+  const template1Handler = () => {
+    setSelectedTemplate(true);
+    html2canvas(refOne.current).then((canvas) => {
+      uploadHandler(canvas.toDataURL());
+    })
+  }
+
+  const template2Handler = () => {
+    setSelectedTemplate(true);
+    html2canvas(refTwo.current).then((canvas) => {
+      console.log(canvas.toDataURL());
+    })
+  }
+
+  const detailsHandler = async () => {
+    setSubmittedForm(true);
+    console.log(getImage)
+  }
+  
+  
+  const finalcardHandler = () => {
+    html2canvas($(finalcardRef.current)[0]).then(canvas => (
+
+      console.log(canvas.toDataURL())
+    ))
+      // try {
+      //   fetch('/api/upload', {
+      //     method: 'POST',
+      //     body: JSON.stringify({ data: getImage }),
+      //     headers: { 'Content-Type': 'application/json' },
+      //   })
+      //     .then((response) => response.json())
+      //     .then((data) => {
+      //        console.log(data.data)
+      //     })
+      // } catch (error) {
+      //   console.error(error);
+      // }
+    
+  }
+
+  const uploadHandler = async (card) => {
+    try {
+      fetch('/api/upload', {
+        method: 'POST',
+        body: JSON.stringify({ data: card }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          backgroundURL = data.data
+          document.getElementById("front").style.backgroundImage = `url(${backgroundURL})`
+          document.getElementById("back").style.backgroundImage = `url(${backgroundURL})`
+        })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <>
+      <TitleContainer>
+        <Title>Design Business card with Nextjs and Emotion css</Title>
+      </TitleContainer>
+      <Container>
+        {selectedtemplate? "selected: "}
+      </Container>
+    </>
+  )
+}
---
+++ .history/pages/index_20220305081437.js
@@ -0,0 +1,64 @@
+
+import { forwardRef, useState, createRef, useEffect, useRef } from 'react';
+import UserDetails from '../components/UserDetails';
+import { Black, Button, Details, FormContainer, Input, Inputs, Label, Blue, Card, Card_1, Card_2, ColorGrid, Container, Dots, Green, Pink, Red1, Red2, Templates, Text, Title, TitleContainer, Yellow, FinalCardContainer, Front, InfoGrid, NameTag, Back, Name, Brand, CardText, Address, PhoneNumber, Email, TextLg, TextSm, UploadButto, Backn, UploadButton } from "../styles/emotion";
+import { FaLaptop, FaMapMarkerAlt, FaPhone } from 'react-icons/fa';
+import { useScreenshot } from 'use-react-screenshot';
+import html2canvas from 'html2canvas';
+
+export default function Home() {
+  const [selectedtemplate, setSelectedTemplate] = useState(false)
+  const [submittedform, setSubmittedForm] = useState(false);
+  const [finalcard, setFinalCard] = useState();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phonenumber, setPhoneNumber] = useState('');
+  const [location, setLocation] = useState('');
+  const [website, setWebsite] = useState('');
+  const [brand, setBrand] = useState('');
+  const [link, setLink] = useState('');
+  const [userprofile, takeScreenshot] = useScreenshot();
+
+  const refOne = createRef();
+  const finalcardRef = useRef();
+
+  const $ = require('jquery')
+
+  let backgroundURL = ""
+  const Card1 = forwardRef((props, ref) => (
+    <Card_1 ref={refOne} id="card" onClick={template1Handler} >
+      <ColorGrid>
+        <Black />
+        <Red1 />
+        <Red2 />
+        <Green />
+      </ColorGrid>
+    </Card_1>
+  ));
+
+ 
+
+  
+  return (
+    <>
+      <TitleContainer>
+        <Title>Design Business card with Nextjs and Emotion css</Title>
+      </TitleContainer>
+      <Container>
+        {selectedtemplate ?
+          <Details>
+            {submittedform ? 
+            "" : 
+}
+
+          </Details>
+           :
+          <Templates >
+            <Text>Select Design </Text>
+            <Card1 ref={refOne} />
+          </Templates>}
+        
+      </Container>
+    </>
+  )
+}
